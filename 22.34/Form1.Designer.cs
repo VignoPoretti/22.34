51e664417fb35908cddbc35d6e8010e419cf1ef4
Utente <Utente@LAPTOP-M4JBAMO1>
2024-05-14 15:35:57 +0200

    Aggiungere i file di progetto.

---
+++ 22.34/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _22._34
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSomma = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxrisultato = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxSomma
+            // 
+            this.textBoxSomma.Location = new System.Drawing.Point(54, 110);
+            this.textBoxSomma.Name = "textBoxSomma";
+            this.textBoxSomma.Size = new System.Drawing.Size(225, 22);
+            this.textBoxSomma.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aggiungi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxrisultato
+            // 
+            this.textBoxrisultato.Location = new System.Drawing.Point(385, 211);
+            this.textBoxrisultato.Name = "textBoxrisultato";
+            this.textBoxrisultato.ReadOnly = true;
+            this.textBoxrisultato.Size = new System.Drawing.Size(209, 22);
+            this.textBoxrisultato.TabIndex = 3;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(556, 48);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 84);
+            this.listBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.textBoxrisultato);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxSomma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSomma;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxrisultato;
+        private System.Windows.Forms.ListBox listBox;
+    }
+}
+
---
+++ 22.34/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._34
+{
+    public partial class Form1 : Form
+    {
+        private List<double> resistenze = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBoxSomma.Text, out double resistenza)) 
+            { 
+                resistenze.Add(resistenza);
+                listBox.Items.Add(resistenza.ToString()); 
+                CalcolaResistenzaTotale();
+                textBoxSomma.Clear();
+            }
+            else
+            {
+                textBoxSomma.Clear();
+            }
+                
+        }
+        private void CalcolaResistenzaTotale()
+        {
+            if (resistenze.Count == 0)
+            {
+                Console.WriteLine("Inserire valori validi");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                double totaleDenominatore = resistenze.Sum(r => 1 / r);
+                double Totale = 1 / totaleDenominatore;
+                textBoxrisultato.Text= Totale.ToString();
+
+            }
+        }
+    }
+}
